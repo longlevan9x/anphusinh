b63407a5557d670ad823e8d6c1ccaced812d9c68
LongPC <vanlong121996@gmail.com>
2018-07-14 14:24:42 +0700

    push to site anphusinh

--- resources/views/website/partials/style.blade.php
+++ resources/views/website/partials/style.blade.php
@@ -1,17 +1,17 @@
 @push('styleMain')
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('font-awesome.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('edua-icons.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('animate.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('owl.carousel.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('owl.transitions.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('cubeportfolio.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('settings.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('bootsnav.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('style.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset_app_css('loader.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('edua-icons.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('animate.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('owl.carousel.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('owl.transitions.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('cubeportfolio.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('settings.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('bootsnav.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset_website_css('loader.css')}}">
 
-    <link rel="icon" href="{{asset_app('images/favicon.png')}}">
+    <link rel="icon" href="{{asset_website('images/favicon.png')}}">
 
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
