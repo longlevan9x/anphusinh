983d472a81bb7b43ba30862cc14d5c9c53eff60e
LongPC <vanlong121996@gmail.com>
2018-08-15 12:10:43 +0700

    update view website/index

--- resources/views/website/partials/aside.blade.php
+++ resources/views/website/partials/aside.blade.php
@@ -53,20 +53,21 @@
     }
 </style>
 <aside class="col-sm-4 wow fadeIn" data-wow-delay="200ms">
-    <div class="widget heading_space">
-        <h3 class="bottom20">@lang('admin/menu.share experience')</h3>
-        @forelse($share_experience as $item)
-            <div class="media">
-                <a class="media-left" style="width: 35%;" href="{{$item->getSlugAndId()}}"><img style="width: 100%;" src="{{$item->getImagePath()}}" alt="post"></a>
-                <div class="media-body">
-                    <h5 class="bottom5">{{$item->title}}</h5>
-                    <a href="#." class="btn-primary btn btn-sm border_radius bottom5">@lang('website.read more')</a>
+    @if(!empty($share_experience))
+        <div class="widget heading_space">
+            <h3 class="bottom20">@lang('admin/menu.share experience')</h3>
+            @forelse($share_experience as $item)
+                <div class="media">
+                    <a class="media-left" style="width: 35%;" href="{{$item->getSlugAndId()}}"><img style="width: 100%;" src="{{$item->getImagePath()}}" alt="post"></a>
+                    <div class="media-body">
+                        <h5 class="bottom5">{{$item->title}}</h5>
+                        <a href="#." class="btn-primary btn btn-sm border_radius bottom5">@lang('website.read more')</a>
+                    </div>
                 </div>
-            </div>
-        @empty
-        @endforelse
-    </div>
-
+            @empty
+            @endforelse
+        </div>
+    @endif
     @if(!empty($advice_expert))
         @php
             $item = $advice_expert->get(0);
