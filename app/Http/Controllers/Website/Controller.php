70d4c0d5f4c066403a11a24b6053f3448fe88005
LongPC <vanlong121996@gmail.com>
2018-08-15 11:44:10 +0700

    update view website/index

--- app/Http/Controllers/Website/Controller.php
+++ app/Http/Controllers/Website/Controller.php
@@ -32,34 +32,40 @@ class Controller extends \App\Http\Controllers\Controller
 		//			$menus = Cache::get('menus');
 		//		} else {
 
+		//		$menus0 = [
+		//			['text' => __('website.home page'), 'url' => url('/')],
+		//			['text' => __('website.product'), 'url' => url('/san-pham')],
+		//		];
+		//
+		//		$menu   = Menu::query()->orderBy('sort_order')->get();
+		//		$menus1 = [
+		//			['text' => __('website.diseases'), 'url' => '#', 'children' => []],
+		//		];
+		//
+		//		$categories            = Category::whereType(Category::TYPE_CATEGORY)->where('parent_id', '<>', 0)->where('is_active', 1)->get();
+		//		$menus1[0]['children'] = $this->getCategoryMenu($categories);
+		//
+		//		$menus2 = [
+		//			['text' => __('website.chuyen-gia'), 'url' => url('/chuyen-gia')],
+		//			['text' => __('website.chia-se'), 'url' => url('/chia-se')],
+		//			['text' => __('website.question answer'), 'url' => url('/hoi-dap')],
+		//			['text' => __('website.news'), 'url' => url('/tin-tuc')],
+		//			['text' => __('website.system_store'), 'url' => url('/he-thong-nha-thuoc')],
+		//			['text' => __('website.order'), 'url' => url('/dat-hang')],
+		//		];
+		//
+		//		$menus = array_merge($menus0, $menus1, $menus2);
+		//		cache()->put('menus', $menus, 60);
+		//		//		}
+		//
 		$menus0 = [
 			['text' => __('website.home page'), 'url' => url('/')],
 			['text' => __('website.product'), 'url' => url('/san-pham')],
 		];
 
-		$menu   = Menu::query()->orderBy('sort_order')->get();
-		$menus1 = [
-			['text' => __('website.diseases'), 'url' => '#', 'children' => []],
-		];
-
-		$categories            = Category::whereType(Category::TYPE_CATEGORY)->where('parent_id', '<>', 0)->where('is_active', 1)->get();
-		$menus1[0]['children'] = $this->getCategoryMenu($categories);
-
-		$menus2 = [
-			['text' => __('website.chuyen-gia'), 'url' => url('/chuyen-gia')],
-			['text' => __('website.chia-se'), 'url' => url('/chia-se')],
-			['text' => __('website.question answer'), 'url' => url('/hoi-dap')],
-			['text' => __('website.news'), 'url' => url('/tin-tuc')],
-			['text' => __('website.system_store'), 'url' => url('/he-thong-nha-thuoc')],
-			['text' => __('website.order'), 'url' => url('/dat-hang')],
-		];
-
-		$menus = array_merge($menus0, $menus1, $menus2);
-		cache()->put('menus', $menus, 60);
-		//		}
-
 		$categories = Category::whereType()->active()->get();
-		$menus      = $this->getCategoryMenu($categories);
+		$menu1      = $this->getCategoryMenu($categories);
+		$menus      = array_merge($menus0, $menus1);
 
 		view()->share(compact('menus'));
 	}
@@ -72,14 +78,14 @@ class Controller extends \App\Http\Controllers\Controller
 	 * @return array
 	 */
 	public function getCategoryMenu($categories, $parent_id = 0, &$output = [], $level = 0) {
-//		foreach ($categories as $category) {
-//			$output[$category->id] = [
-//				'text' => $category->name,
-//				'url'  => url($category->slug),
-//			];
-//		}
-//
-//		return $output;
+		//		foreach ($categories as $category) {
+		//			$output[$category->id] = [
+		//				'text' => $category->name,
+		//				'url'  => url($category->slug),
+		//			];
+		//		}
+		//
+		//		return $output;
 		foreach ($categories as $category) {
 			/** @var Category $category */
 			if ($category->parent_id == $parent_id) {
@@ -91,7 +97,8 @@ class Controller extends \App\Http\Controllers\Controller
 					];
 				}
 				else {
-					$output[$category->parent_id]['url']                     = "#";
+					$output[$category->parent_id]['url'] = "#";
+
 					$output[$category->parent_id]['children'][$category->id] = [
 						'text' => $category->name,
 						'url'  => url($category->slug)
