ea3cd436bc860bfd0b1bf447e61e941a230db0fd
LongPC <vanlong121996@gmail.com>
2018-08-15 11:42:30 +0700

    update view website/index

--- app/Http/Controllers/Website/Controller.php
+++ app/Http/Controllers/Website/Controller.php
@@ -28,35 +28,39 @@ class Controller extends \App\Http\Controllers\Controller
 	 * @throws \Exception
 	 */
 	public function getMenu() {
-//		if (cache()->has('menus')) {
-//			$menus = Cache::get('menus');
-//		} else {
+		//		if (cache()->has('menus')) {
+		//			$menus = Cache::get('menus');
+		//		} else {
 
-			$menus0 = [
-				['text' => __('website.home page'), 'url' => url('/')],
-				['text' => __('website.product'), 'url' => url('/san-pham')],
-			];
+		$menus0 = [
+			['text' => __('website.home page'), 'url' => url('/')],
+			['text' => __('website.product'), 'url' => url('/san-pham')],
+		];
 
-			$menu = Menu::query()->orderBy('sort_order')->get();
-			$menus1 = [
-				['text' => __('website.diseases'), 'url' => '#', 'children' => []],
-			];
+		$menu   = Menu::query()->orderBy('sort_order')->get();
+		$menus1 = [
+			['text' => __('website.diseases'), 'url' => '#', 'children' => []],
+		];
 
-			$categories            = Category::whereType(Category::TYPE_CATEGORY)->where('parent_id', '<>', 0)->where('is_active', 1)->get();
-			$menus1[0]['children'] = $this->getCategoryMenu($categories);
+		$categories            = Category::whereType(Category::TYPE_CATEGORY)->where('parent_id', '<>', 0)->where('is_active', 1)->get();
+		$menus1[0]['children'] = $this->getCategoryMenu($categories);
 
-			$menus2 = [
-				['text' => __('website.chuyen-gia'), 'url' => url('/chuyen-gia')],
-				['text' => __('website.chia-se'), 'url' => url('/chia-se')],
-				['text' => __('website.question answer'), 'url' => url('/hoi-dap')],
-				['text' => __('website.news'), 'url' => url('/tin-tuc')],
-				['text' => __('website.system_store'), 'url' => url('/he-thong-nha-thuoc')],
-				['text' => __('website.order'), 'url' => url('/dat-hang')],
-			];
+		$menus2 = [
+			['text' => __('website.chuyen-gia'), 'url' => url('/chuyen-gia')],
+			['text' => __('website.chia-se'), 'url' => url('/chia-se')],
+			['text' => __('website.question answer'), 'url' => url('/hoi-dap')],
+			['text' => __('website.news'), 'url' => url('/tin-tuc')],
+			['text' => __('website.system_store'), 'url' => url('/he-thong-nha-thuoc')],
+			['text' => __('website.order'), 'url' => url('/dat-hang')],
+		];
 
-			$menus = array_merge($menus0, $menus1, $menus2);
-			cache()->put('menus', $menus, 60);
-//		}
+		$menus = array_merge($menus0, $menus1, $menus2);
+		cache()->put('menus', $menus, 60);
+		//		}
+
+		$categories = Category::whereType()->active()->get();
+		$menus      = $this->getCategoryMenu($categories);
+
 		view()->share(compact('menus'));
 	}
 
@@ -68,13 +72,14 @@ class Controller extends \App\Http\Controllers\Controller
 	 * @return array
 	 */
 	public function getCategoryMenu($categories, $parent_id = 0, &$output = [], $level = 0) {
-		foreach ($categories as $category) {
-			$output[$category->id] = [
-				'text'     => $category->name,
-				'url'      => url($category->slug),
-			];
-		}
-		return $output;
+//		foreach ($categories as $category) {
+//			$output[$category->id] = [
+//				'text' => $category->name,
+//				'url'  => url($category->slug),
+//			];
+//		}
+//
+//		return $output;
 		foreach ($categories as $category) {
 			/** @var Category $category */
 			if ($category->parent_id == $parent_id) {
@@ -84,7 +89,8 @@ class Controller extends \App\Http\Controllers\Controller
 						'url'      => url($category->slug),
 						'children' => []
 					];
-				} else {
+				}
+				else {
 					$output[$category->parent_id]['url']                     = "#";
 					$output[$category->parent_id]['children'][$category->id] = [
 						'text' => $category->name,
@@ -102,7 +108,6 @@ class Controller extends \App\Http\Controllers\Controller
 	}
 
 
-
 	public function getBannerBottomLeft() {
 		/** @var PostMeta $model */
 		$model = PostMeta::where('key', PostMeta::KEY_IS_BANNER)->first();
@@ -111,7 +116,8 @@ class Controller extends \App\Http\Controllers\Controller
 
 		if (empty($model) || empty($post)) {
 			$banner_bottom_left = [];
-		} else {
+		}
+		else {
 			/** @var Post $post */
 
 			$banner_bottom_left = [
@@ -134,7 +140,8 @@ class Controller extends \App\Http\Controllers\Controller
 		$pathInfo = str_replace('/', '', $pathInfo);
 		if (in_array($pathInfo, ['san-pham', 'he-thong-nha-thuoc', 'dat-hang', 'tin-tuc', 'hoi-dap', 'chuyen-gia', 'dat-hang-thanh-cong'])) {
 			$this->breadcrumb = [['text' => __('website.' . $pathInfo), 'url' => '#']];
-		} else {
+		}
+		else {
 			$prefix = $this->prefixBreadcrumb;
 			if ($this->prefixBreadcrumb == Post::TYPE_NEWS) {
 				$prefix = 'tin-tuc';
@@ -166,7 +173,8 @@ class Controller extends \App\Http\Controllers\Controller
 	public function getShareAside() {
 		if (cache()->has('share_experience')) {
 			$share_experience = Cache::get('share_experience');
-		} else {
+		}
+		else {
 			$share_experience = Post::whereType(Post::TYPE_SHARE)->orderByDesc('created_at')->limit(5)->get();
 			cache()->put('share_experience', $share_experience, 60);
 		}
@@ -176,7 +184,8 @@ class Controller extends \App\Http\Controllers\Controller
 	public function getAdviceAside() {
 		if (cache()->has('advice_expert')) {
 			$advice_expert = Cache::get('advice_expert');
-		} else {
+		}
+		else {
 			$advice_expert = Post::whereType(Post::TYPE_ADVICE)->orderByDesc('created_at')->limit(5)->get();
 			cache()->put('advice_expert', $advice_expert, 60);
 		}
