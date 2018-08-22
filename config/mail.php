488e57878658803610c8fb6936307dae2fa8bb3b
LongPC <vanlong121996@gmail.com>
2018-08-22 15:20:35 +0700

    update update seo

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'vanlong121996@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'congtymailinh585@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
