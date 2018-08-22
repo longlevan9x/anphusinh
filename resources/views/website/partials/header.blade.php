798808d64f5a6e268473818593b8f814f79fddfd
LongPC <vanlong121996@gmail.com>
2018-08-22 17:48:20 +0700

    update update seo

--- resources/views/website/partials/header.blade.php
+++ resources/views/website/partials/header.blade.php
@@ -4,8 +4,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="keywords" content="{{setting("seo_keyword")}}" />
-    <meta name="description" content="{{setting("seo_description")}}" />
+    <meta name="keywords" content="{{$seo_keyword}}" />
+    <meta name="description" content="{{$seo_description}}" />
     <title>{{setting(KEY_WEBSITE_NAME)}} - {{setting(KEY_WEBSITE_DESCRIPTION)}}</title>
     <meta property="fb:app_id" content="{{ env('FB_APP_ID') ?? '' }}" />
     <meta property="og:url" content="{{ Request::url() }}" />
